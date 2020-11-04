1940686bcb6ec0228ed9b821a3d301f9ff987069
newcomb-luke <newcomb.luke@protonmail.com>
2020-11-04 13:19:04 -0500

    Added boilerplate code and added clap

---
+++ src/lib.rs
@@ -0,0 +1,53 @@
+use clap::ArgMatches;
+use std::error::Error;
+
+pub static VERSION: &'static str = "0.1.0";
+
+pub fn run(config: &CLIConfig) -> Result<(), Box<dyn Error>> {
+    if !config.file_path.ends_with(".kasm") {
+        return Err(format!(
+            "Input file must be a KASM file. Found: {}",
+            config.file_path
+        )
+        .into());
+    }
+
+    let mut output_path = config.output_path_value.clone();
+
+    // If the output path was not specified
+    if output_path.is_empty() {
+        // Create a new string the same as file_path
+        output_path = config.file_path.clone();
+
+        // Replace the file extension of .kasm with .ko
+        output_path.replace_range((output_path.len() - 4).., "ko");
+    } else if !output_path.ends_with(".ko") {
+        output_path.push_str(".ko");
+    }
+
+    if config.debug {
+        println!("Outputting to: {}", output_path);
+    }
+
+    Ok(())
+}
+
+pub struct CLIConfig {
+    pub file_path: String,
+    pub output_path_value: String,
+    pub debug: bool,
+}
+
+impl CLIConfig {
+    pub fn new(matches: ArgMatches) -> CLIConfig {
+        CLIConfig {
+            file_path: String::from(matches.value_of("INPUT").unwrap()),
+            output_path_value: if matches.is_present("output_path") {
+                String::from(matches.value_of("output_path").unwrap())
+            } else {
+                String::new()
+            },
+            debug: matches.is_present("debug"),
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use clap::{App, Arg};
+use std::process;
+
+use kasm::{run, CLIConfig};
+
+fn main() {
+    let matches = App::new("Kerbal Assembler")
+        .version(kasm::VERSION)
+        .author("Luke Newcomb")
+        .about("Assembles KerbalAssembly files into KerbalObject files to be linked.")
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Sets the input file to use")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("output_path")
+                .help("Sets the output file to use")
+                .short("o")
+                .long("output")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("debug")
+                .help("Displays debugging information during the assembly process.")
+                .short("d")
+                .long("debug"),
+        )
+        .get_matches();
+
+    let config = CLIConfig::new(matches);
+
+    if let Err(e) = run(&config) {
+        eprintln!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
